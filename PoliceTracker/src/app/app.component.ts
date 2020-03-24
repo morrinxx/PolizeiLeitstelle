1267c2725b2253db34fe3914eeb81fae22f7902c
Felix <felixbogi@gmail.com>
2020-03-24 12:25:14 +0100

    New

--- PoliceTracker/src/app/app.component.ts
+++ PoliceTracker/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
+
+declare var google: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'PoliceTracker';
+export class AppComponent implements AfterViewInit {
+  @ViewChild("mapContainer", { static: false }) gmap: ElementRef;
+  map: google.maps.Map;
+  lat = 40.73061;
+  lng = -73.935242;
+
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8
+  };
+
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map
+  });
+
+  ngAfterViewInit() {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    this.marker.setMap(this.map);
+  }
 }
