ce79657e8c8f7b70cb580fbaa5d06a426217d0a0
Marcel <marcel.poelzl@hotmail.com>
2020-05-22 14:59:20 +0200

    Added status buttons

---
+++ MobileApp/app/src/main/java/at/htlleonding/policemobileclient/AuthenticationFragment.kt
@@ -0,0 +1,61 @@
+package at.htlleonding.policemobileclient
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.biometric.BiometricPrompt
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import at.htlleonding.policemobileclient.databinding.FragmentAuthenicationBinding
+import kotlinx.android.synthetic.main.fragment_authenication.*
+import java.util.concurrent.Executors
+
+
+class AuthenticationFragment : Fragment() {
+    private val executor = Executors.newSingleThreadExecutor()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val bP = BiometricPrompt(this, executor, object: BiometricPrompt.AuthenticationCallback(){
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                activity!!.runOnUiThread {
+                    Toast.makeText(activity, getString(R.string.main_successfulAuthentication), Toast.LENGTH_LONG).show()
+                }
+                bt_authentication_login.findNavController().navigate(R.id.action_authenicationFragment_to_mainFragment)
+            }
+
+
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                if(errorCode != BiometricPrompt.ERROR_NEGATIVE_BUTTON){
+                    activity!!.runOnUiThread {
+                        Toast.makeText(activity, getString(R.string.main_authenticationError), Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
+            override fun onAuthenticationFailed() {
+                activity!!.runOnUiThread {
+                    Toast.makeText(activity, getString(R.string.main_authenticationFailed), Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle(getString(R.string.authenticate))
+            .setNegativeButtonText(getString(R.string.cancel))
+            .build()
+
+        val binding: FragmentAuthenicationBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_authenication, container, false
+        )
+        binding.btAuthenticationLogin.setOnClickListener { bP.authenticate(promptInfo) }
+        return binding.root
+    }
+}
--- MobileApp/app/src/main/java/at/htlleonding/policemobileclient/MainActivity.kt
+++ MobileApp/app/src/main/java/at/htlleonding/policemobileclient/MainActivity.kt
@@ -1,5 +1,6 @@
 package at.htlleonding.policemobileclient
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -11,46 +12,16 @@ import kotlinx.android.synthetic.main.fragment_authenication.*
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
-    companion object{
-        lateinit var bP: BiometricPrompt
-        lateinit var promptInfo: BiometricPrompt.PromptInfo
-    }
 
     private lateinit var navController: NavController
-    private val executor = Executors.newSingleThreadExecutor()
+    companion object{
+        fun showToast(context: Context?, text:String){
+            Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        bP = BiometricPrompt(this, executor, object: BiometricPrompt.AuthenticationCallback(){
-            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                super.onAuthenticationSucceeded(result)
-                runOnUiThread {
-                    Toast.makeText(this@MainActivity, getString(R.string.main_successfulAuthentication), Toast.LENGTH_LONG).show()
-                }
-
-            }
-
-
-            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                super.onAuthenticationError(errorCode, errString)
-                if(errorCode != BiometricPrompt.ERROR_NEGATIVE_BUTTON){
-                    runOnUiThread {
-                        Toast.makeText(this@MainActivity, getString(R.string.main_authenticationError), Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-
-            override fun onAuthenticationFailed() {
-                runOnUiThread {
-                    Toast.makeText(this@MainActivity, getString(R.string.main_authenticationFailed), Toast.LENGTH_LONG).show()
-                }
-            }
-        })
-        promptInfo = BiometricPrompt.PromptInfo.Builder()
-            .setTitle(getString(R.string.authenticate))
-            .setNegativeButtonText(getString(R.string.cancel))
-            .build()
     }
 }
